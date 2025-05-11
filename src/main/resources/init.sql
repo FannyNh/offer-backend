514c24bbed8c746dac0f7495c82389ad62156a39
FannyNh <thethirdmoira@gmail.com>
2025-05-11 20:08:37 +0200

    init sql db

---
+++ src/main/resources/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    group_id BIGINT
+);
